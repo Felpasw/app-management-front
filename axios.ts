25b8db524b30881a7e2e70582e6dcb45e7bd7bf6
Felpasw <felipecavalcantelacerda@hotmail.com>
2024-09-21 15:05:22 -0300

    API url mocado temporariamente para testes

--- axios.ts
+++ axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = `${process.env.NEXT_BASE_API_URL ? process.env.NEXT_BASE_API_URL : 'http://localhost:4000'}` 
+const baseURL = `https://app-management-api.onrender.com` 
 
 
 const axiosInstance = axios.create({
