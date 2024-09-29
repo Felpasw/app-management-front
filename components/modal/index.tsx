c60d5d605c2e1e7e7cc8c13a97af49a1fd2f5fb1
Felpasw <felipecavalcantelacerda@hotmail.com>
2024-09-29 15:15:04 -0300

    adicionado animaçaõ de modal

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { IoMdClose } from 'react-icons/io';
 
 interface modalProps {
@@ -7,17 +8,32 @@ interface modalProps {
 }
 
 export default function Modal({ toggle, isOpen, children }: modalProps) {
+  const [visible, setVisible] = useState(false);
+  const [render, setRender] = useState(isOpen);
+
+  useEffect(() => {
+    if (isOpen) {
+      setRender(true); 
+      setTimeout(() => setVisible(true), 100); 
+    } else {
+      setVisible(false); 
+      setTimeout(() => setRender(false), 300); 
+    }
+  }, [isOpen]);
+
   return (
     <>
-      {isOpen ? (
-        <div className="bg-black/30 backdrop-blur-sm h-full w-full z-[300] fixed top-0 left-0  flex justify-center items-center">
-          <div className=" p-6 bg-white border border-gray-200 rounded-lg relative w-[35%]">
-            <IoMdClose className="absolute top-0 right-0 text-3xl cursor-pointer" onClick={() => toggle()} />
+      {render && (
+        <div className="bg-black/30 backdrop-blur-sm h-full w-full z-[300] fixed top-0 left-0 flex justify-center items-center transition-opacity duration-300 ease-out">
+          <div
+            className={`p-6 bg-white border border-gray-200 rounded-lg relative w-[35%] transform transition-transform duration-300 ease-out ${
+              visible ? 'scale-100' : 'scale-0'
+            }`}
+          >
+            <IoMdClose className="absolute top-0 right-0 text-3xl cursor-pointer" onClick={toggle} />
             {children}
           </div>
         </div>
-      ) : (
-        <></>
       )}
     </>
   );
